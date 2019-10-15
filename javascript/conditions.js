54f3431fe4dbad8f983666699ed657b749f605a1
TTENDO <ttendobecky@gmail.com>
2019-10-15 12:34:29 +0300

    adding the functions written in es6 in exames6.js and more conditions

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -41,7 +41,7 @@ let greaterPaye = (salary1,salary2)=>{
 }
 
 greaterPaye(500000,300000);*/
-
+/* 
 let compute=(sal1,sal2)=>{
     if (sal1>=130000 && sal2>=130000){
         let paye1 = sal1 * (30/100);
@@ -55,4 +55,46 @@ let compute=(sal1,sal2)=>{
     }
 }
 
-compute(500000,500000);
+compute(500000,500000);*/
+
+
+//Using the else if
+/*var mysal = 200;
+var yoursal = 1000
+if (mysal  > yoursal){
+    console.log('mysal is greater than your sal')
+}
+else if(mysal<yoursal){
+    console.log('My salary is less than your salary')
+}*/
+
+
+//Using a switch
+/*
+var sal = 20000;
+switch(sal){
+    case 2000:
+        console.log('That is not 20000');
+        break;
+    case 200000:
+        console.log('That is not 20000');
+        break;
+    default:
+        console.log('20000 is the default value');
+}*/
+
+//printing odd numbers btn 0 and 5
+/*
+var num = 0
+for (var num = 0; num  <=5;num++){
+    
+    if (num%2==1){
+    console.log(num);}
+}*/
+
+//printing even numbers 
+
+for (let a=0;a<=5;a++){
+    var c = (a%2==0)?a:
+    console.log(c)
+}
--- javascript/newFunctions.js
+++ javascript/newFunctions.js
@@ -3,6 +3,7 @@ let abc = ()=>{} INSTEAD OF function abc(){}
 let abc = (x,y,z)=>{} INSTEAD OF function abc(x,y,z){}
 */
 
+/*
 let printName = (name)=>{
 console.log('name is '+name);
 }
@@ -13,4 +14,5 @@ let num =(a,b) =>{
     let c = a+b;
     console.log(c);
 }
-num(2,5);
+num(2,5);*/
+
