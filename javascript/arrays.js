e4d7e90321559352f8c7adb87b9d755e2558c7d4
TTENDO <ttendobecky@gmail.com>
2019-10-21 13:25:18 +0300

    arrays and objects

---
+++ javascript/arrays.js
@@ -0,0 +1,15 @@
+/*let oranges = [20,50,[40,50],90];
+console.log(oranges[2][1]);//finds the position of 509*/
+
+//a loop that will loop through the array and print all items
+/*var mangoes = [20,40,60,80];
+
+for(var item=0;item<4;item++){
+    console.log(mangoes[item]);
+}*/
+
+
+
+
+
+
---
+++ javascript/objects.js
@@ -0,0 +1,44 @@
+//--------------METHODS-----------------------//
+
+//structsured programming-have a problem, break it down and solve it
+//ability of an object to hide its properties from the rest is encapsulation
+//overriding is redefining a method with the same name but different implementations
+//overloading is when two or more methods in one class have the same method name but different parameters
+//polymorphism is when classes or methods can take up one or more forms
+//constructor is a special method that is used to initialise an instatianted object
+
+//Method style
+/*var student = {
+    name: 'Patrick',
+    age: 50,
+    gender: 'M'
+
+}
+
+console.log(student.name);*/
+
+//another style with a constructor
+/*function student(x,y,z){
+    this.name = x;
+    this.age = y;
+    this.dob = z;
+}
+
+//creating an instance of student using the new key word
+ becky = new student('becky',23,'14th March');
+ console.log(becky.name);*/
+
+
+
+
+/*function school(a,b){
+    this.name=a,
+    this.motto=b
+   
+}
+
+becky = new school("mengo","akaa");
+console.log(becky.name);*/
+
+
+//my five objects
